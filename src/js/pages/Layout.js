a27b7ba8d9d37d8a85ede18b0dc14247d0d8dc3e
asherjames <asherjames.nz@gmail.com>
2016-04-07 13:34:31 +1200

    Add css

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router";
 import RaisedButton from "material-ui/lib/raised-button";
 import FlatButton from "material-ui/lib/flat-button";
+import "../../style/style.css";
 
 export default class Layout extends React.Component {
 
@@ -12,10 +13,12 @@ export default class Layout extends React.Component {
     };
     return(
       <div>
-        <h1>Seikkor</h1>
-        <Link to="photos"><FlatButton label="Photos" style={style}/></Link>
-        <Link to="about"><FlatButton label="About" style={style}/></Link>
-        {this.props.children}
+          <h1 id="maintitle">Seikkor</h1>
+          <div id="navbuttons">
+            <Link to="photos"><FlatButton label="Photos" style={style}/></Link>
+            <Link to="about"><FlatButton label="About" style={style}/></Link>
+          </div>
+          {this.props.children}
       </div>
     );
   }
