539920a5e82324a9e2f7604d6ca69e9f0de048c7
asherjames <asherjames.nz@gmail.com>
2016-04-07 21:47:09 +1200

    Begin implementing Promise in Photos.js

--- src/js/pages/Photos.js
+++ src/js/pages/Photos.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { PhotoSwipeGallery } from "react-photoswipe";
+import RSVP from "rsvp";
 
 import "react-photoswipe/dist/photoswipe.css";
 
 export default class Photos extends React.Component {
   constructor() {
     super();
-    this.images = {
-      items: [
-        {
-          src: 'http://lorempixel.com/1500/1920/nature',
-          thumbnail: 'http://lorempixel.com/150/200/nature',
-          w: 1500,
-          h: 2000,
-          title: 'Image title 1'
-        },
-        {
-          src: 'http://lorempixel.com/1400/1920/nature',
-          thumbnail: 'http://lorempixel.com/180/150/nature',
-          w: 1500,
-          h: 2000,
-          title: 'Image title 2'
-        },
-      ],
-      options: {}
-    };
+    this.state = { images:[]}
+  }
+
+  componentDidMount() {
+    
+  }
+
+  getImageInfo() {
+    url = "http://localhost:8080/seikkor/photo/allphotoinfo";
+    let promise = new RSVP.Promise((resolve, reject) => {
+      let client = new XMLHttpRequest();
+      client.open("GET", url);
+      client.onreadystatechange = handler;
+      client.responseType = "json";
+      client.setRequestHeader("Accept", "application/json");
+      client.send();
+    })
   }
 
   getThumbnailContent = (item) => {
